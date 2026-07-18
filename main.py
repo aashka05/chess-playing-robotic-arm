aa6676a983e8800e33fdb7c202934fb01f296395
Veera Agrawal <veera.it.bvm@gmail.com>
2026-07-18 19:17:39 +0530

    model integration
    1. create the project structure
    
    chess_engine/
    config/
    robotic_arm/
    utils/
    vision/
    	data/
    	models/
    	outputs/
    	test_images/
    	board_mapper.py
    	camera.py
    	detect_image.py
    	detect_video.py
    main.py
    requirements.py
    README.md
    
    2. create venv (python -m venv venv), then install dependencies (pip install ultralytics opencv-python numpy matplotlib python-chess stockfish)
    
    3. from https://huggingface.co/dopaul/chess-piece-detector-merged-v2/tree/main, download the best.pt file (this is pre-trained model that will be used for piece detection)
    
    4. place this file in vision/models/
    
    5. now we need some test images to see if the model is working well
    
    6. as decided earlier, if the current model works well work is done, if not, we will need to train the model using the dataset of our own project images

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import chess
+from stockfish import Stockfish
+from ultralytics import YOLO
+
+print("OpenCV:", cv2.__version__)
+print("NumPy:", np.__version__)
+print("python-chess: Installed")
+print("Stockfish wrapper: Installed")
+print("Ultralytics YOLO: Installed")
+
+print("\nEverything is installed correctly!")
